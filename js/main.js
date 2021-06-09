9f3f70c82d204872757d991dbecbe0f81e69c904
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-06-09 14:47:30 -0700

    Completed issue-1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Declare a variable to append my DOM tree too.
 var $appended = document.querySelector('.appended');
-// var $mobileAppended = document.querySelector('.mobileAppended');
+var $mobileAppended = document.querySelector('.mobileAppended');
+
 // Function to create the DOM tree, as well as add their respective element property to the proper td.
 function renderElements(element) {
   var trAppended = document.createElement('tr');
@@ -14,27 +15,27 @@ function renderElements(element) {
   tdTwo.setAttribute('class', 'letters');
   tdTwo.textContent = element.name + ' ' + element.symbol;
   var tdThree = document.createElement('td');
-  tdThree.setAttribute('class', 'numbers');
-  tdThree.textContent = Math.ceil(element.price * 1000) / 1000;
+  tdThree.setAttribute('class', 'numbers text-align-center');
+  tdThree.textContent = Math.ceil(element.price * 100) / 100;
   var tdFour = document.createElement('td');
-  tdFour.setAttribute('class', 'numbers');
-  tdFour.textContent = Math.ceil(element.volume) + element.symbol;
+  tdFour.setAttribute('class', 'numbers text-align-center');
+  tdFour.textContent = '$' + Math.ceil(element.volume * 100) / 100;
   var tdFive = document.createElement('td');
-  tdFive.setAttribute('class', 'numbers');
+  tdFive.setAttribute('class', 'numbers text-align-center');
   tdFive.textContent = '$' + Math.ceil(element.marketCap * 100) / 100;
   var tdSix = document.createElement('td');
-  tdSix.setAttribute('class', 'numbers');
-  tdSix.textContent = element.totalSupply + ' ' + element.symbol;
+  tdSix.setAttribute('class', 'numbers text-align-center');
+  tdSix.textContent = element.priceChange1h + '%';
   var tdSeven = document.createElement('td');
-  tdSeven.setAttribute('class', 'numbers');
-  tdSeven.textContent = element.priceChange1h + '%';
+  tdSeven.setAttribute('class', 'numbers text-align-center');
+  tdSeven.textContent = element.priceChange1d + '%';
   var tdEight = document.createElement('td');
-  tdEight.setAttribute('class', 'numbers');
-  tdEight.textContent = element.priceChange1d + '%';
+  tdEight.setAttribute('class', 'numbers text-align-center');
+  tdEight.textContent = element.priceChange1w + '%';
   var tdNine = document.createElement('td');
   var tdNineSticky = document.createElement('i');
   tdNineSticky.setAttribute('class', 'fas fa-sticky-note');
-
+  // console.log(element.priceChange1w);
   trAppended.appendChild(tdOne);
   tdOne.appendChild(tdOneImage);
   trAppended.appendChild(tdTwo);
@@ -48,12 +49,43 @@ function renderElements(element) {
   tdNine.appendChild(tdNineSticky);
   return trAppended;
 }
+
 // Function to create Mobile DOM Tree
-// function mobileRenderElements(mobileElement) {
-//   var divOne = document.createElement('div');
-//   divOne.setAttribute('class', 'flex');
-//   divOne.setAttribute('class', 'justify-center');
-// }
+function mobileRenderElements(mobileElement) {
+  var divOne = document.createElement('div');
+  divOne.setAttribute('class', 'flex justify-center selector');
+  var divTwo = document.createElement('div');
+  divTwo.setAttribute('class', 'mobileSelector align-center padding-left');
+  var imgOne = document.createElement('img');
+  imgOne.setAttribute('class', 'mobileLogo');
+  imgOne.setAttribute('src', mobileElement.icon);
+  var divThree = document.createElement('div');
+  divThree.setAttribute('class', 'mobileSelector align-center padding-left');
+  var h3One = document.createElement('h3');
+  h3One.setAttribute('class', 'letters');
+  h3One.textContent = mobileElement.symbol;
+  var h3Two = document.createElement('h3');
+  h3Two.setAttribute('class', 'letters');
+  h3Two.textContent = mobileElement.name;
+  var divFour = document.createElement('div');
+  divFour.setAttribute('class', 'mobileSelector align-center padding-left padding-bottom');
+  var h3Three = document.createElement('h3');
+  h3Three.setAttribute('class', 'numbers');
+  h3Three.textContent = '$' + Math.ceil(mobileElement.price * 100) / 100;
+  var h3Four = document.createElement('h3');
+  h3Four.setAttribute('class', 'numbers');
+  h3Four.textContent = mobileElement.priceChange1d + '%';
+
+  divOne.appendChild(divTwo);
+  divTwo.appendChild(imgOne);
+  divOne.appendChild(divThree);
+  divThree.appendChild(h3One);
+  divThree.appendChild(h3Two);
+  divOne.appendChild(divFour);
+  divFour.appendChild(h3Three);
+  divFour.appendChild(h3Four);
+  return divOne;
+}
 // Function to make our API request, and then append our DOM tree to our targeted position.
 function coinstatRequest() {
   var xhr = new XMLHttpRequest();
@@ -72,7 +104,29 @@ function coinstatRequest() {
   });
   xhr.send();
 }
+// Function to make the API request, and append our DOM tree for mobile only.
+function mobileCoinStatRequest() {
+  var xhrm = new XMLHttpRequest();
+  xhrm.open('GET', 'https://api.coinstats.app/public/v1/coins?skip=0&limit=7&currency=USD');
+  xhrm.setRequestHeader('token', 'abc123');
+  xhrm.responseType = 'json';
+  xhrm.addEventListener('load', function () {
+    // console.log('What is received:' + xhrm.response);
+    // console.log('An array of what is received', xhrm.response.coins);
+    // console.log('The Index 0 of this Array: ', xhrm.response.coins[0]);
+    // console.log('The current price of Index 0: ' + xhrm.response.coins[0].price);
+    for (var i = 0; i < xhrm.response.coins.length; i++) {
+      var test = mobileRenderElements(xhrm.response.coins[i]);
+      $mobileAppended.append(test);
+    }
+  });
+  xhrm.send();
+}
 // When the window loads, will load the coinstatRequest function.
 window.addEventListener('DOMContentLoaded', function (event) {
   coinstatRequest();
+});
+// When the window loads, will load the mobileCoinStatRequest function
+window.addEventListener('DOMContentLoaded', function (event) {
+  mobileCoinStatRequest();
 });
