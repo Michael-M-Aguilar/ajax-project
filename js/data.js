852ecf3710952f7a1c864b53a98f3313053bef83
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-06-10 11:12:16 -0700

    Completed HTML and CSS for Modal.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,15 @@
 /* exported data */
+var noteEntry = {
+  entries: [],
+  editing: null
+};
+
+var toJSON = localStorage.getItem('local-storage');
+if (toJSON !== null) {
+  noteEntry = JSON.parse(toJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var toJSONOld = JSON.stringify(noteEntry);
+  localStorage.setItem('local-storage', toJSONOld);
+});
